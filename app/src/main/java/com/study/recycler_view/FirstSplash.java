90faa306107a0f2efed29e4fa15a5cbda3877a02
jioo123 <aaaqqqaqaq12@naver.com>
2019-07-02 13:36:09 +0900

    first splash

---
+++ app/src/main/java/com/study/recycler_view/FirstSplash.java
@@ -0,0 +1,25 @@
+package com.study.recycler_view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class FirstSplash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(FirstSplash.this,SplashActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },900);
+        //0.9초로 저장
+
+    }
+}
